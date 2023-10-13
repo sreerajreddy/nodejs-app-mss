8887af545be3e30e98084f0dba62d3995d427451
sreerajreddy <139043578+sreerajreddy@users.noreply.github.com>
2023-10-13 12:06:36 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.235.247.238:9000/',
+  serverUrl: 'http://172.31.33.5:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_c870b0745ec382363f3f2e73f272bae9f64d9440',
+	    'sonar.login': 'squ_c870b0745ec382363f3f2e73f272bae9f64d9440', 
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
